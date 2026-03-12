69760a7e641d92203bec3e2515b737b203f196fb
Cverly <anej.cverle@gmail.com>
2026-03-12 14:33:44 +0100

    FM-39 Sistem zahtev / povpraševanj

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from app.api.routes.test_router import test_router
 from app.api.routes.auth_router import AuthRouter
+from app.api.routes.inquiry_router import inquiry_router
 
 
 # from app.api.routes import auth, users, items
@@ -12,3 +13,4 @@ auth_router = AuthRouter().router
 
 api_router.include_router(test_router)
 api_router.include_router(auth_router)
+api_router.include_router(inquiry_router)
